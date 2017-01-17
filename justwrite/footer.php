997e4b948a83540c9dee045c9ab32e82d768ae3f
MithileshCParab <parab.mithilesh09@gmail.com>
2017-01-18 00:19:20 +0530

    Juswrite wordpress theme

---
+++ justwrite/footer.php
@@ -0,0 +1,14 @@
+<footer id="main-footer" class="footer-wrap clearfix">
+    		<aside class="footer-credits">
+        		<a href="http://parallelclassroom.com" title="Parallel Classroom" rel="nofollow" class="blog-title">Parallel Classroom</a>
+            	<strong class="copyright">Copyright 2015 Parallel Classroom. All rights reserved.</strong>
+                 
+                                <span class="theme-author">
+                    <a href="http://twinsdohtml.com/" target="_blank">Proudly powered by TDH</a>
+                                   
+				</span>
+                </aside><!-- END .footer-credits -->
+			<a href="#" class="back-to-top"><i class="fa fa-angle-up"></i> </a>
+		</footer><!-- END .footer-wrap -->
+</body>
+</html>
---
+++ justwrite/sidebar.php
@@ -0,0 +1,17 @@
+<?php
+/* ------------------------------------------------------------------------- *
+ *	Sidebar template				
+/* ------------------------------------------------------------------------- */
+?>
+ 
+<section class="sidebar clearfix">
+   <h1 style="font-weight:bold;margin:10px;font-size:36px;text-align:center;"> Follow Us </h1>
+	<aside id="ac_social_buttons_widget-3" class="side-box clearfix widget ac-social-buttons-widget">
+        <div class="sb-content clearfix">        
+        <ul class="sidebar-social clearfix">
+        	<li class="alignleft"><a href="https://twitter.com/" class="social-btn twitter">Twitter<i class="fa fa-twitter"></i> </a></li><li class="alignright"><a href="https://www.facebook.com/" class="social-btn facebook">Facebook<i class="fa fa-facebook"></i> </a></li><li class="alignleft"><a href="https://plus.google.com/" class="social-btn google-plus">Google +<i class="fa fa-google-plus"></i> </a></li><li class="alignright"><a href="http://www.youtube.com/" class="social-btn youtube">Youtube<i class="fa fa-youtube"></i> </a></li>        </ul>
+        
+        </div></aside><!-- END .sidebox .widget -->
+
+</section><!-- END .sidebar -->    
+   
